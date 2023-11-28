9bd3bfed59f598bfb9d645e141db47f223be94f8
Roger Filomeno <rpfilomeno@gmail.com>
2023-11-28 11:52:41 +0800

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info < (3, 9):
 
 setup(
     name='ETS2/ATS Telemetry Overlay',
-    version='0.1.0',
+    version='0.2.0',
     description='Provides an overlay of iomportant truck telemetry',
     author='@notatallshaw',
     url='https://github.com/rpfilomeno/ETS2-ATS-Telemetry-Overlay.git',
